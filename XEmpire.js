8a34b15e3c6d0e09a9f706a4b505b323e4add91a
Yaseen673 <pwkdk245@gmail.com>
2024-09-30 01:04:45 +0300

    Update XEmpire.js

--- XEmpire.js
+++ XEmpire.js
@@ -12,7 +12,7 @@ const headers = (apiKey, apiTime, apiHash) => ({
     "Api-Time": apiTime,
     "Origin": "https://game.muskempire.io",
     "Referer": "https://game.muskempire.io/",
-    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1"
+    "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Mobile Safari/537.36"
 });
 
 const auth = async (initData) => {
